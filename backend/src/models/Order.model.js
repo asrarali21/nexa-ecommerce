bf368334ab88e25ba722847a2b473a1e5fcb8f24
asrarali21 <mohdasrar3825@gmail.com>
2025-09-26 19:37:44 +0530

    fix: correct order schema definition and add missing fields

--- backend/src/models/Order.model.js
+++ backend/src/models/Order.model.js
@@ -9,5 +9,31 @@ const orderSchema = new Schema({
      product :{
         type : mongoose.Schema.Types.ObjectId,
         ref:"Product"
+    },
+    amount :{
+       type : Number
+    },
+    PaymentStatus : {
+      type : String,
+      emum : ["PENDING","SUCCESS","FAILED"],
+      default : "PENDING"
+    },
+     razorpayOrderId: { 
+    type: String
+ },
+  razorpayPaymentId: {
+     type: String 
+    },
+  razorpaySignature: {
+     type: String 
+    },
+    PaidAt : {
+        type:Date
     }
+
+    
 }, {timestamps:true})
+
+
+
+export const Order = mongoose.model("Order" , orderSchema)
