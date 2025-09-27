ae4c7b4de70e51b19ebf3cacf9660b9c8cda83e2
asrarali21 <mohdasrar3825@gmail.com>
2025-09-27 23:34:48 +0530

    feat: add payment verification functionality and update order status

--- backend/src/controller/payment.controller.js
+++ backend/src/controller/payment.controller.js
@@ -3,6 +3,7 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { razorpay } from "../utils/razorpay.js";
+import crypto from "crypto"
 
 
 const CreateOrderRazorpay = asyncHandler(async(req , res)=>{
@@ -47,7 +48,38 @@ const CreateOrderRazorpay = asyncHandler(async(req , res)=>{
 
 })
 
+const verifyPayment = asyncHandler (async(req ,res)=>{
+  
+    const {razorpay_payment_id ,razorpay_order_id , razorpay_signature , orderId} = req.body
+
+
+    const order = await Order.findById(orderId)
+   if (!order) {
+     throw new ApiError(404 , "Order not found")
+   }
+
+   const generated_signature = crypto.createHmac("sha256" , process.env.RAZORPAY_KEY_SECRET)
+   .update(razorpay_order_id + "|" + razorpay_payment_id)
+   .digest("hex")
+
+   if (!generated_signature) {
+    throw new ApiError(400 , "payment verification failed")
+   }
+
+   if (generated_signature === razorpay_signature) {
+       order.PaymentStatus = "SUCCESS"
+       order.razorpayPaymentId = razorpay_payment_id,
+       order.razorpaySignature = razorpay_signature
+       await order.save()
+   }
+
+
+   res.status(200)
+   .json(new ApiResponse(200 , order , "payment verified successfully"))
+
+})
 
 
 
-export {CreateOrderRazorpay}
+
+export {CreateOrderRazorpay , verifyPayment}
