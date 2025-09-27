01f40a4b0a46beb908a3673f384e967a4a2428ba
asrarali21 <mohdasrar3825@gmail.com>
2025-09-27 21:27:18 +0530

    fix: correct order ID reference and update order creation response structure

--- backend/src/controller/payment.controller.js
+++ backend/src/controller/payment.controller.js
@@ -1,4 +1,5 @@
 import { Order } from "../models/Order.model.js";
+import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { razorpay } from "../utils/razorpay.js";
@@ -6,27 +7,47 @@ import { razorpay } from "../utils/razorpay.js";
 
 const CreateOrderRazorpay = asyncHandler(async(req , res)=>{
      
-    const {orderID} = req.body
+    const {orderId} = req.body
 
-   const order = await Order.findById(orderID)
+   const order = await Order.findById(orderId)
+
+   if (!order) {
+      throw new ApiError(400 , "Cloud not find order")
+   }
+
+
 
     const options ={
-        amount : order.amount,
-        cucurrency : order.currency,
+        amount : order.amount * 100,
+        currency : order.currency,
         receipt: order._id.toString()
     }
 
+    console.log(options);
+    
+
 
     const razorpayOrder = await razorpay.orders.create(options);
+
+    console.log("razorpay order",razorpayOrder);
+    
 
     order.razorpayOrderId = razorpayOrder.id
 
     await order.save();
 
+    const RazorpayOrderDetails ={
+      razorpayOrder,
+      orderId:order._id
+
+    }
+
     res.status(200)
-    .json(new ApiResponse(200 , razorpayOrder , "succesfully Razorpay Order Created"))
+    .json(new ApiResponse(200 , RazorpayOrderDetails , "succesfully Razorpay Order Created"))
 
 })
+
+
 
 
 export {CreateOrderRazorpay}
